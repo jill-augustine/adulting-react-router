ed4fa0ee1ddd046ac38a84e8f911cd446360f33a
Jill Augustine <37906581+jill-augustine@users.noreply.github.com>
2025-07-11 00:06:48 +0200

    feat: add ui card components

---
+++ app/components/ui/boopsize-card.tsx
@@ -0,0 +1,21 @@
+import {Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "~/components/ui/card";
+import { type BoopSize, data } from "~/lib/boop-sizes";
+
+const BoopSizeCard = ({boopSize}: {boopSize: BoopSize}) => {
+  return (
+    <Card key={boopSize.id}>
+      <CardHeader>
+        <CardTitle>{boopSize.name}</CardTitle>
+        <CardDescription>BoopSize</CardDescription>
+        {/*<CardAction>Card Action</CardAction>*/}
+      </CardHeader>
+      {/*<CardContent>*/}
+      {/*  <p>Size: {boopSize.name} ({boopSize.value})</p>*/}
+      {/*</CardContent>*/}
+      {/*<CardFooter>*/}
+      {/*  <p>Card Footer</p>*/}
+      {/*</CardFooter>*/}
+    </Card>
+  )
+}
+export { BoopSizeCard }
---
+++ app/components/ui/tasktype-card.tsx
@@ -0,0 +1,48 @@
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "~/components/ui/card";
+// import { type TaskType, getTaskTypesForTaskType}from "~/lib/taskTypes";
+import { type TaskType } from "~/lib/task-types";
+
+const TaskTypeSummaryCard = ({taskType}: {taskType: TaskType}) => {
+  return (
+    <Card key={taskType.id}>
+      <CardHeader>
+        <CardTitle>TaskType Summary</CardTitle>
+        {/*<CardAction>Card Action</CardAction>*/}
+      </CardHeader>
+      <CardContent>
+        <CardTitle>#{taskType.id} {taskType.name}</CardTitle>
+        <CardDescription>
+          {JSON.stringify(taskType.boopSize)}<br/><br/>
+        </CardDescription>
+      </CardContent>
+      {/*<CardFooter>*/}
+      {/*  <p>Card Footer</p>*/}
+      {/*</CardFooter>*/}
+    </Card>
+  )
+}
+
+const TaskTypeDetailsCard = ({taskType}: {taskType: TaskType}) => {
+  return (
+    <Card key={taskType.id}>
+      <CardHeader>
+        <CardTitle>TaskType Details</CardTitle>
+        {/*<CardAction>Card Action</CardAction>*/}
+      </CardHeader>
+      <CardContent>
+        <CardTitle>#{taskType.id} {taskType.name}</CardTitle>
+        <CardDescription>
+          {JSON.stringify(taskType.boopSize)}<br/><br/>
+        </CardDescription>
+      </CardContent>
+      {/*<CardFooter>*/}
+      {/*  <p>Card Footer</p>*/}
+      {/*</CardFooter>*/}
+    </Card>
+  )
+}
+
+export {
+  TaskTypeDetailsCard,
+  TaskTypeSummaryCard,
+}
