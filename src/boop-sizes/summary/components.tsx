9cea03244027f22eb8c1fdcd67c7ca22653527f6
Jill Augustine <37906581+jill-augustine@users.noreply.github.com>
2025-08-05 13:58:21 +0200

    feat: Add boop size selectors

--- src/boop-sizes/summary/components.tsx
+++ src/boop-sizes/summary/components.tsx
@@ -5,8 +5,27 @@ import {
   CardTitle
 } from "@/components/ui/card.tsx";
 import {type BoopSize} from "@/boop-sizes/service";
+import {Button} from "@/components/ui/button";
+import {Command, CommandGroup, CommandList, CommandItem, CommandEmpty} from "@/components/ui/command.tsx";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import * as React from "react";
+import {ChevronsUpDown} from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import {useState} from "react";
+import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
+import {Label} from "@/components/ui/label";
 
-const Components = ({boopSize}: { boopSize: BoopSize }) => {
+const SummaryCard = ({boopSize}: { boopSize: BoopSize }) => {
   return (
     <Card key={boopSize.id}>
       <CardHeader>
@@ -23,4 +42,88 @@ const Components = ({boopSize}: { boopSize: BoopSize }) => {
     </Card>
   )
 }
-export {Components}
+
+type BoopSizeSelectorProps = {
+  boopSizes: BoopSize[];
+  boopSizeName: string;
+  setBoopSizeName: (value: string) => void;
+}
+
+export const BoopSizeSelectorRadio = ({boopSizes, boopSizeName, setBoopSizeName}: BoopSizeSelectorProps) => {
+  return (
+    <RadioGroup onValueChange={setBoopSizeName} value={boopSizeName}>
+      {
+        boopSizes.map((boopSize: BoopSize) => (
+          <div className="flex items-center gap-3" key={boopSize.id}>
+            <RadioGroupItem key={boopSize.id} value={boopSize.name} id={`boop-size-${boopSize.name}`}/>
+            <Label htmlFor={`boop-size-${boopSize.name}`}>{boopSize.name}</Label>
+          </div>
+        ))
+      }
+    </RadioGroup>
+  )
+}
+
+export const BoopSizeSelectorSelect = ({boopSizes}: { boopSizes: BoopSize[] }) => {
+  const [boopValue, setBoopValue] = useState("");
+  return (
+    <Select value={boopValue} onValueChange={setBoopValue}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme"/>
+      </SelectTrigger>
+      <SelectContent>
+        {boopSizes.map((boopSize: BoopSize) => (
+          <SelectItem key={boopSize.id} value={boopSize.name}>
+            {boopSize.name}
+          </SelectItem>)
+        )}
+      </SelectContent>
+    </Select>
+  )
+}
+
+export const BoopSizeSelectorPopover = ({boopSizes}: { boopSizes: BoopSize[] }) => {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? boopSizes.find((boopSize) => String(boopSize.value) === value)?.name
+            : "Select task type size..."}
+          <ChevronsUpDown className="opacity-50"/>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandEmpty>No Task Type sizes found.</CommandEmpty>
+            <CommandGroup>
+              {boopSizes.map(boopSize => (
+                <CommandItem
+                  key={boopSize.id}
+                  value={String(boopSize.value)}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue)
+                    setOpen(false)
+                  }}
+                >
+                  {boopSize.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+export {SummaryCard}
