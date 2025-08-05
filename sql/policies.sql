d5dc69da8f7b43aa40266cc1dc7946c522654ec4
Jill Augustine <37906581+jill-augustine@users.noreply.github.com>
2025-08-05 14:01:08 +0200

    feat: Add boop-size table policy

--- sql/policies.sql
+++ sql/policies.sql
@@ -10,7 +10,18 @@ create policy "authenticated non-anonymous insert"
 
 create policy "authenticated non-anonymous insert"
     on "public"."task_types"
+    as PERMISSIVE
+    for INSERT
     to authenticated
     with check (
+    (((auth.jwt() ->> 'is_anonymous'::text))::boolean = false)
+    );
+
+create policy "authenticated non-anonymous select"
+    on "public"."boop_sizes"
+    as PERMISSIVE
+    for SELECT
+    to authenticated
+    using (
     (((auth.jwt() ->> 'is_anonymous'::text))::boolean = false)
     );
