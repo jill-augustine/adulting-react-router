a4d35f14ceae3e9da215a233dad0a7834330d682
Jill Augustine <37906581+jill-augustine@users.noreply.github.com>
2025-08-31 19:56:20 +0200

    feat: Remove unneeded SQL tables

--- sql/tables/create.sql
+++ sql/tables/create.sql
@@ -1,59 +1,90 @@
--- Create Task types table
-create TABLE task_types
+-- Normal (non-join) tables created in order of dependencies
+-- CREATE TABLE public.boop_sizes
+-- (
+--     name    text                                       NOT NULL UNIQUE,
+--     value   integer                                    NOT NULL,
+--     id      UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL
+-- );
+-- ALTER TABLE boop_sizes
+--     ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE public.chores
 (
-    id           BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name         TEXT UNIQUE NOT NULL,
-    boop_size_id INT         NOT NULL REFERENCES boop_sizes (id),
-    user_id      UUID DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE,
-    frequency    TEXT        NOT NULL
+    name        text                                         NOT NULL,
+    description text,
+    id          UUID PRIMARY KEY DEFAULT (gen_random_uuid()) NOT NULL,
+    user_id     UUID             DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE,
+    task_types  JSONB,
+    is_hidden   boolean          DEFAULT false
 );
+ALTER TABLE chores
+    ENABLE ROW LEVEL SECURITY;
 
--- Create Join table (many:many) between chores and task types
-CREATE TABLE join_chore_task_types
+create TABLE public.tags
 (
-    chore_id     BIGINT REFERENCES chores (id) ON DELETE CASCADE,
-    task_type_id INT REFERENCES task_types (id) ON DELETE CASCADE,
-    user_id      UUID DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE,
-    PRIMARY KEY (chore_id, task_type_id)
+    id        UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name      TEXT UNIQUE NOT NULL,
+    user_id   UUID             DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE,
+    is_hidden boolean          DEFAULT false
+
 );
+ALTER TABLE tags
+    ENABLE ROW LEVEL SECURITY;
 
-
--- Create tags table
-create TABLE tags
+create TABLE public.task_types
 (
-    id      BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name    TEXT UNIQUE NOT NULL,
-    user_id UUID DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE
+    id        UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name      TEXT UNIQUE NOT NULL,
+    chore_id  UUID        NOT NULL REFERENCES chores (id) ON DELETE CASCADE,
+    user_id   UUID             DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE,
+    frequency TEXT        NOT NULL,
+    is_hidden boolean          DEFAULT false
 );
+ALTER TABLE task_types
+    ENABLE ROW LEVEL SECURITY;
 
--- Create Join table (many:many) between chores and tags
-CREATE TABLE join_chore_tags
+CREATE TABLE tasks
 (
-    chore_id BIGINT REFERENCES chores (id) ON DELETE CASCADE,
-    tag_id   INT REFERENCES tags (id) ON DELETE CASCADE,
+    id             UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    task_type_id   UUID NOT NULL REFERENCES task_types (id),
+    chore_id       UUID NOT NULL REFERENCES chores (id) ON DELETE CASCADE,
+    start_date     DATE NOT NULL,
+    due_date       DATE NOT NULL,
+    date_completed DATE,
+    completed_by   UUID REFERENCES tasks (id), -- Self-referencing FK
+    tags           UUID REFERENCES tags (id),
+    user_id        UUID             DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE
+);
+ALTER TABLE tasks
+    ENABLE ROW LEVEL SECURITY;
+
+-- Join tables (for many:many joins)
+CREATE TABLE public.chore_tags_join
+(
+    chore_id UUID REFERENCES chores (id) ON DELETE CASCADE,
+    tag_id   UUID REFERENCES tags (id) ON DELETE CASCADE,
     user_id  UUID DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE,
     PRIMARY KEY (chore_id, tag_id)
 );
+ALTER TABLE chore_tags_join
+    ENABLE ROW LEVEL SECURITY;
 
--- Create Join table (many:many) between task_types and tags
-CREATE TABLE join_task_types_tags
-(
-    task_type_id BIGINT REFERENCES task_types (id) ON DELETE CASCADE,
-    tag_id       INT REFERENCES tags (id) ON DELETE CASCADE,
-    user_id      UUID DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE,
-    PRIMARY KEY (task_type_id, tag_id)
-);
+-- CREATE TABLE public.chore_task_types_join
+-- (
+--     chore_id     UUID REFERENCES chores (id) ON DELETE CASCADE,
+--     task_type_id UUID REFERENCES task_types (id) ON DELETE CASCADE,
+--     user_id      UUID DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE,
+--     PRIMARY KEY (chore_id, task_type_id)
+-- );
+-- ALTER TABLE chore_task_types_join
+--     ENABLE ROW LEVEL SECURITY;
 
--- Create task table and link (1:1) to chores and task types
-CREATE TABLE tasks
-(
-    id             BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    task_type_id   INT    NOT NULL REFERENCES task_types (id),
-    chore_id       BIGINT NOT NULL REFERENCES chores (id) ON DELETE CASCADE,
-    start_date     DATE   NOT NULL,
-    due_date       DATE   NOT NULL,
-    date_completed DATE,
-    completed_by   BIGINT REFERENCES tasks (id), -- Self-referencing FK
-    tags           INT REFERENCES tags (id),
-    user_id        UUID DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE
-);
+-- CREATE TABLE tags_task_types_join
+-- (
+--     task_type_id UUID REFERENCES task_types (id) ON DELETE CASCADE,
+--     tag_id       UUID REFERENCES tags (id) ON DELETE CASCADE,
+--     user_id      UUID DEFAULT (auth.uid()) REFERENCES auth.users (id) ON DELETE CASCADE,
+--     PRIMARY KEY (task_type_id, tag_id)
+-- );
+-- ALTER TABLE tags_task_types_join
+--     ENABLE ROW LEVEL SECURITY;
